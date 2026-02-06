0d447a55ce2bd399babfd4752e0d1f095f92122c
232Ahmed232 <x99aliahmed@gmail.com>
2026-02-06 23:14:36 +0500

    sewt up

--- src/app/(app)/voter/[category]/page.tsx
+++ src/app/(app)/voter/[category]/page.tsx
@@ -1,14 +1,47 @@
 'use client'
+import axios from 'axios';
 import { useParams } from 'next/navigation'
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 
 function page() {
-    const params = useParams()
+  const params = useParams()
 
-    console.log(params);
+  console.log(params);
+  const [list, setList] = useState([])
 
-    useEffect(()=>{},[])
-    
+  useEffect(() => {
+    const getdata = async () => {
+
+      if (params.category == "actor") {
+        const respose = await axios.get("/api/get-voteActor")
+        if (respose.data.success) {
+          setList(respose.data.message);
+        }
+      }
+
+
+
+      else if (params.category == "actress") {
+        const respose = await axios.get("/api/get-votedFemaleactors")
+        if (respose.data.success) {
+          setList(respose.data.message);
+        }
+      }
+        else if (params.category == "director") {
+          const respose = await axios.get("/api/get-votedirector")
+          if (respose.data.success) {
+            setList(respose.data.message);
+          }
+
+        }
+
+
+        }
+        getdata()
+      }, [])
+
+      console.log(list);
+      
   return (
     <div>{params.category}</div>
   )
